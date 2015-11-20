30260a9367daf87418bf8a935f9de11b47c47a8d
RunshengSong <srs90324@gmail.com>
2015-11-19 17:12:00 -0800

    CS174A Final Project
    Nov 19 init by Runsheng Song

---
+++ main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  CS174A_final_project
+//
+//  Created by Runsheng Song on 15/11/19.
+//
+
+#include <stdio.h>
+#include "paillier.h"
+
+int main(void) {
+    int c = add(1,2);
+    printf("%i",c);
+}
