9c85ad28592fb455b1304ac919fa691f11f19360
Yiting Ju <yiting.ju@gmail.com>
2015-11-22 17:27:09 -0800

    Create create_tables.sql
    This file is for creating all tables
    -- Final Project for CMPSC 174A
    -- Yiting Ju & Runsheng Song Nov.22

---
+++ create_tables.sql
@@ -0,0 +1,13 @@
+-- This file is for creating all tables
+-- Final Project for CMPSC 174A
+-- Yiting Ju & Runsheng Song Nov.22
+
+use projects;
+
+-- Create Table Employees
+CREATE TABLE Employees(
+	id			integer,
+    age			integer,
+    salary		TEXT NOT NULL,
+    PRIMARY KEY(id)
+);
