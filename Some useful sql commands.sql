c0a3bd08c50f7d2b1e3761cb633e97754ecd0f37
Yiting Ju <yiting.ju@gmail.com>
2015-12-04 13:36:49 -0800

    Update Some useful sql commands.sql

--- Some useful sql commands.sql
+++ Some useful sql commands.sql
@@ -5,6 +5,7 @@ CREATE DATABASE project;
 
 # grant all privileges to user:yiting with ip_address:128.111.106.188 with password:roma
 grant all on *.* to yiting@'128.111.106.188' identified by 'roma';
+grant all on project.* to batigoal@'%' identified by 'clicc';
 
 # show all grants/privileges
 select * from information_schema.user_privileges;
